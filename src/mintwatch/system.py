240884ac817587b1072ec9024338a116319b7d7a
Thainara Oliveira <thainarasdo.23@gmail.com>
2026-09-05 00:28:31 -0300

    feat: adding get_uptime()

--- src/mintwatch/system.py
+++ src/mintwatch/system.py
@@ -15,3 +15,15 @@ def get_os_name() -> str:
     result = get_os_info()
 
     result["NAME"]
+
+def get_uptime() -> str:
+    os_uptime = Path("/proc/uptime")
+
+    uptime_seconds = float(os_uptime.read_text().split(" ")[0])
+
+    hours = int(uptime_seconds // 3600)
+    remaining_seconds = uptime_seconds % 3600
+    minutes = int(remaining_seconds // 60)
+    seconds = int(remaining_seconds % 60)
+
+    return f"{hours}h {minutes}m {seconds}s"
